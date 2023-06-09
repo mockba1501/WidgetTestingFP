e49835b6c62c3f901daedddd471f67fe8da29ea7
Mohammed Ockba <7790976+mockba1501@users.noreply.github.com>
2023-06-09 17:06:34 +0300

    Count Down Timer
    Added Count Down Timer To project

---
+++ Source/WidgetTestingFP/CountDown.cpp
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CountDown.h"
+#include <Components/TextRenderComponent.h>
+
+// Sets default values
+ACountDown::ACountDown()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	CountDownText = CreateDefaultSubobject<UTextRenderComponent>(TEXT("CountDownNumber"));
+	CountDownText->SetHorizontalAlignment(EHTA_Center);
+	CountDownText->SetWorldSize(150.0f);
+	
+	RootComponent = CountDownText;
+	CountDownTime = 3;
+}
+
+// Called when the game starts or when spawned
+void ACountDown::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	GetWorldTimerManager().SetTimer(CountDownTimerHandle, this, &ACountDown::AdvanceTimer, 1.0f, true);
+}
+
+// Called every frame
+void ACountDown::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ACountDown::UpdateTimerDisplay()
+{
+	CountDownText->SetText(FText::AsNumber(FMath::Max(CountDownTime, 0))); //FString::FromInt Not working no conversion from 
+}
+
+void ACountDown::AdvanceTimer()
+{
+	CountDownTime--;
+	UpdateTimerDisplay();
+	if (CountDownTime < 1)
+	{
+		//Stop running the timer
+		GetWorldTimerManager().ClearTimer(CountDownTimerHandle);
+		CountDownHasFinished();
+	}
+}
+
+void ACountDown::CountDownHasFinished()
+{
+	CountDownText->SetText(FText::FromString("Done Counting!!"));
+}
+
---
+++ Source/WidgetTestingFP/CountDown.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Countdown.generated.h"
+
+UCLASS()
+class WIDGETTESTINGFP_API ACountDown : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACountDown();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	int32 CountDownTime;
+	UTextRenderComponent* CountDownText;
+
+	void UpdateTimerDisplay();
+
+	void AdvanceTimer();
+
+	void CountDownHasFinished();
+
+	FTimerHandle CountDownTimerHandle;
+};
